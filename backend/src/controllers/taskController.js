1cde9aa85c243da2cae94ff018ffb12e186356bf
unknown <lira.soaresprofissional@gmail.com>
2025-03-26 22:25:52 -0300

    added controllers files

---
+++ backend/src/controllers/taskController.js
@@ -0,0 +1,22 @@
+import tasksModel from "../models/tasksModel.js";
+
+const getAll = async (req, res) => {
+  const tasks = await tasksModel.getAll();
+  return res.status(200).json(tasks);
+};
+const createTask = async (req, res) => {
+  const createdTask = await tasksModel.createTask(req.body);
+  return res.status(201).json(createdTask);
+};
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  await tasksModel.deleteTask(id);
+  return res.status(204).json("Message: deleted");
+};
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  await tasksModel.updateTask(id, req.body);
+  return res.status(204).json("message: Updated");
+};
+
+export { getAll, createTask, deleteTask, updateTask };

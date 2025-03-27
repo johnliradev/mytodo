c1a038ac8e033f4241c508d36d11d9164aabf283
unknown <lira.soaresprofissional@gmail.com>
2025-03-26 22:16:14 -0300

    added model files

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,40 @@
+import connection from "./connection.js";
+
+const getAll = async () => {
+  const [tasks] = await connection.execute("SELECT * FROM tasks");
+  return tasks;
+};
+
+const createTask = async (task) => {
+  const { title } = task;
+  const dateUTC = new Date(Date.now()).toUTCString();
+
+  const query = "INSERT INTO tasks(title, status, created_at) VALUES (?,?,?)";
+
+  const [createTask] = await connection.execute(query, [
+    title,
+    "pending",
+    dateUTC,
+  ]);
+
+  return { insertId: createTask.insertId };
+};
+
+const deleteTask = async (id) => {
+  const [removedTask] = await connection.execute(
+    "DELETE FROM tasks WHERE id = ?",
+    [id],
+  );
+  return removedTask;
+};
+
+const updateTask = async (id, task) => {
+  const { title, status } = task;
+
+  const query = "UPDATE tasks SET title = ?, status = ? WHERE id = ?";
+
+  const [updatedTask] = await connection.execute(query, [title, status, id]);
+  return updatedTask;
+};
+
+export { getAll, createTask, deleteTask, updateTask };
